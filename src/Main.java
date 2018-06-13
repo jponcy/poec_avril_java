3638e8df7af18af1dd7c9cd91c91b3d28ab6bb70
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2018-06-13 15:23:06 +0200

    for plus avance

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 3; i <= 10; ++ i) {
-            System.out.println(i);
-        }
+        int i = 2;
 
-        System.out.println("Fini");
+        for (int j = 1; j <= 4; ++ j, i *= j);
+
+        System.out.println(i);
     }
 }
