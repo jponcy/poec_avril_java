c5e99fd7dc3d3491fb3594e128b8da80773c2788
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2018-06-13 14:54:06 +0200

    ternaire

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        short i = 2, j = 4;
-        i = 2;
-        i = j = 3;
+        short i;
 
-        switch (i) {
-            case 3:
-                System.out.println("J'avais 3 pommes");
-                break;
-            case 2:
-                System.out.println("Pas si mal !");
-                break;
-            default:
-                System.out.println(":'(");
-                break;
+        if (3 == 4) {
+            i = 3;
+        } else {
+            i = 2;
         }
+
+        i = (3 == 4 ? 3 : 2);
     }
 }
