b1c1727db271dbcce801568adea5b495a49936a4
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2018-06-13 15:12:34 +0200

    for simple

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        boolean finished = false;
-        short i = 3;
-        int j;
+        for (int i = 3; i <= 10; ++ i) {
+            System.out.println(i);
+        }
 
-        do {
-            j = i % 10;
-            i ++;
-            finished = j == 3;
-        } while (!finished);
-
-        System.out.println(j);
+        System.out.println("Fini");
     }
 }
